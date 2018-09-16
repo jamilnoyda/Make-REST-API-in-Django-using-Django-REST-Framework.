c2ab5ff5b77b91c88f6342cbce17e2952d3933a2
Jamil Noyda <jamil_noyda@gridle.io>
2018-09-16 11:30:33 +0530

    settings updated

--- RESTAPI_app/settings.py
+++ RESTAPI_app/settings.py
@@ -117,7 +117,6 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
 
-
 USE_I18N = True
 
 USE_L10N = True
@@ -127,14 +126,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# STATIC_URL = '/static/'
 STATIC_URL = '/os.path.join(BASE_DIR, "static")/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/accounts/'
 
+
+# login urls 
+
+LOGIN_REDIRECT_URL = '/accounts/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
+
+# media urls
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
